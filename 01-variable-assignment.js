fdcb14794c7eac2626667f19e86479debbff8c6c
Dhanshrisiddh <dhanshrisiddh0910@gmail.com>
2024-10-23 14:15:48 +0530

    01-variable-assignment
    01-variable-assignment

---
+++ 01-variable-assignment.js
@@ -0,0 +1,5 @@
+console.log("City Name : Kirloskarwadi");
+console.log("Best Friend Name : Aarathi");
+console.log("Country Name : India");
+console.log("School Name : Swami Ramanadnagar Vidyalay,Ramanandnagar");
+console.log("College Name : Rajarambapu Institute of Technolgoy,Islampur");
