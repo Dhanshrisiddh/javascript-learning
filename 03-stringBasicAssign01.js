7b287ff32c878034d00a73e4f2066aa7f9bfe13a
Dhanshrisiddh <dhanshrisiddh0910@gmail.com>
2024-10-25 22:21:22 +0530

    03-stringBasicAssign.js
    03-stringBasicAssign.js

---
+++ 03-stringBasicAssign01.js
@@ -0,0 +1,22 @@
+
+
+console.log("============ With no argument no return type ===========");
+
+function stringBasic(){
+    console.log("My Dream Company is: Google");
+   
+var hobby1 = "Watching Movie";                  
+var hobby2 = "Painting";
+var hobby3 = "Travelling";
+console.log(`My Hobbies are ${hobby1},${hobby2},${hobby3}`);
+var hobby = hobby1 + hobby2 + hobby3;
+var hobbyLength = hobby.length;
+console.log("Total number of characters is: ",hobbyLength);
+
+}
+stringBasic();
+
+
+
+
+
